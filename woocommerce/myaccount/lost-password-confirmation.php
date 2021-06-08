0fa82415f32e222bb06dde4e563bbef3d1c70bcb
STProjectPeople <tomasz.sikora@projectpeople.pl>
2021-06-08 21:37:33 +0200

    lost password styling

--- woocommerce/myaccount/lost-password-confirmation.php
+++ woocommerce/myaccount/lost-password-confirmation.php
@@ -22,6 +22,8 @@ wc_print_notice( esc_html__( 'Password reset email has been sent.', 'woocommerce
 
 <?php do_action( 'woocommerce_before_lost_password_confirmation_message' ); ?>
 
-<p><?php echo esc_html( apply_filters( 'woocommerce_lost_password_confirmation_message', esc_html__( 'A password reset email has been sent to the email address on file for your account, but may take several minutes to show up in your inbox. Please wait at least 10 minutes before attempting another reset.', 'woocommerce' ) ) ); ?></p>
-
+<p class="lost-password__text"><?php echo esc_html( apply_filters( 'woocommerce_lost_password_confirmation_message', esc_html__( 'A password reset email has been sent to the email address on file for your account, but may take several minutes to show up in your inbox. Please wait at least 10 minutes before attempting another reset.', 'woocommerce' ) ) ); ?></p>
+<div class="lost-password__link-wrapper">
+    <a class="lost-password__link" href="<?php echo home_url();?>">Wróć na stronę główną</a>
+</div>
 <?php do_action( 'woocommerce_after_lost_password_confirmation_message' ); ?>
